e283d57599638241529fcb6539ca0ef3a32d5e20
alvera-v <alvera-v@student.42malaga.com>
2025-02-10 09:25:41 +0100

    working to disappear the balloons

--- src/index.js
+++ src/index.js
@@ -13,18 +13,25 @@ function displayPoem(response) {
   //poemElement.innerHTML = response.data.answer;
 }
 
-function generatePoem(event) {
-  event.preventDefault();
-
+function generatePoem() {
   let context =
-    "I need a short poem about friendship, not longer than 4 lines. Separate each line with <br />. Do not include a title to the poem. Make sure to sign at the end as Your Dear Friend. Just write the poem";
+    "I need a short poem about friendship, not longer than 4 lines. Separate each line with <br />. Do not include a title to the poem. Make sure to sign at the end as Your Dear Friend inside a <strong> element. Just write the poem";
   let prompt = "Write a short poem";
   let apiKey = "131b90447daa3e3cfco5aa3tc6e2b482";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let poemElement = document.querySelector("#poem");
 
   axios.get(apiUrl).then(displayPoem);
 }
 
-let balloonElement = document.querySelector("#balloon");
-balloonElement.addEventListener("click", generatePoem);
+function removeBalloon(event, balloonElement) {
+  event.preventDefault();
+
+  balloonElement.classList.add("hidden");
+
+  generatePoem();
+}
+
+let balloonElementOne = document.querySelector("#balloon");
+let ballonElementTwo = document.querySelector("#balloon-2");
+balloonElementOne.addEventListener("click", removeBalloon, balloonElementOne);
+ballonElementTwo.addEventListener("click", removeBalloon, ballonElementTwo);
